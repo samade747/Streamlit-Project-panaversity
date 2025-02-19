3b938c772e96c0c2cebc0301169c814cb355da5c
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-02-19 11:17:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
-form io import BytesIO
+from io import BytesIO
 
 
 
@@ -14,5 +14,38 @@ uploaded_files = st.file_uploader("Choose a file", type=["csv", "xlsx"],
 accept_multiple_files=True)
 
 if uploaded_files:
-    for uploaded_file in uploaded_files:                       )
-        file_ext = os.path.splitext(file.name)[-1].lower()
+    for file in uploaded_files:                       )
+    file_ext = os.path.splitext(file.name)[-1].lower()
+
+
+    if file_ext == ".csv":
+        df = pd.read_csv(file)
+    elif file_ext == ".xlsx":
+        df = pd.read_excel(file)
+    else:
+        st.error("Unsupported file type: {file_ext}")
+        continue
+
+    #Display infor about the life
+    st.write(f"**File name:** {file.name}")
+    st.write(f"**File size:** {file.size/1024}")
+
+    #show 5 rows our df
+    st.write("Preview the Head of the Dataframe")
+    st.dataframe(df.head())
+
+    #options for data cleaning
+    st.subheader("Data Cleaning Options")
+    if st.checkbox(f"clean Data for {file.name}"):
+        col1, col2 = st.columns(2)
+
+        with col1:
+            if st.button(f"Remove Duplicates for {file.name}"):
+                df.drop_duplicates(inplace=True)
+                st.write("Duplicates removed")
+
+        with col2:
+            if st.button(f"fill missing values for {file.name}"):
+               numeric_cols = df.select_dtypes(include=["numbers"]).columns
+               df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+               st.write("Missing values filled")
